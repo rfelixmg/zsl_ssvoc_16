5b08587aecbf5cc679f76f9f9de9e7bc4c73c17a
Rafael Felix <rfelixmg@gmail.com>
2017-02-17 15:30:09 +1030

    SigalLayer

--- tmp.py
+++ tmp.py
@@ -1,51 +1,74 @@
-def euclidean_distance(y_true, y_pred):
-    from keras.optimizers import K
-    return K.sqrt(K.sum(K.square(y_true - y_pred), axis=1))
+import os
 
-def distance(y_true, y_pred):
-    return np.sqrt(np.sum(np.square(y_true - y_pred), axis=1))
+os.environ['THEANO_FLAGS'] += ",exception_verbosity='high'"
+
+import keras.backend as K
+import numpy as np
+from keras.engine.topology import Layer
+from keras.layers import Input, Dense
+from keras.optimizers import RMSprop
+from keras.models import Model
+import numpy as np
 
 def nothing_to_lose(y_true, y_pred):
     return y_pred
 
-def mode_euclidean_distance(inputs):
-    (y_true, y_pred) = inputs
-    from keras.optimizers import K
-    return K.sqrt(K.sum(K.square(y_true - y_pred), axis=1))
+class SigalLoss(Layer):
+    def __init__(self,  Ws, Wt, C, alpha,**kwargs):
+        self.Ws     = K.variable(Ws, name='Ws')
+        self.Wt     = K.variable(Wt, name='Ws')
+        self.C      = K.variable(C, name= 'C')
+        self.alpha  = K.variable(alpha, name= 'C')
+        super(SigalLoss, self).__init__(**kwargs)
+
+    def euclidean_distance(self, y_true, y_pred):
+        return K.sqrt(K.sum(K.square(y_true - y_pred), axis=0))
+
+    def vector_distance(self, y_true, y_pred):
+        return K.sqrt(K.sum(K.square(y_true - y_pred)))
+
+    def build(self, input_shape):
+        super(SigalLoss, self).build(input_shape)
+
+    def call(self, inputs, mask=None):
+        X, A = inputs
+
+        D = self.vector_distance(X, A)
+
+        Xt = X.repeat(self.Wt.shape[0].eval(), axis=0)
+        Dt = self.euclidean_distance(self.Wt, Xt)
+
+        Xs = X.repeat(self.Ws.shape[0].eval(), axis=0)
+        Ds = self.euclidean_distance(self.Ws, Xs)
+
+        MS = 0.5 * K.sum(self.C + 0.5 * D - 0.5 * Ds)
+        MV = 0.5 * K.sum(self.C + 0.5 * D - 0.5 * Dt)
+
+        return (self.alpha * D) + ((1 - self.alpha)*(MS + MV))
+
+    def get_output_shape_for(self, input_shape):
+        return (1,1)
 
 
-from keras.layers import Input, Dense, Merge, Lambda, merge, Flatten
-from keras.models import Model
-
-import numpy as np
-
-# First model (MLP from CNN -> semantic_embedding space)
 input_x = Input(shape=(4096,))
-f_x = Dense(300,
-            name='W_embedding',
-            activation='linear')(input_x)
-visual = Model(input=input_x, output=f_x)
-
-# Second model (linear transformation from GloVe -> semantic_embedding space)
 input_u = Input(shape=(300,))
-g_u = Dense(300,
-            name='V_embedding',
-            init='identity',
-            activation='linear')(input_u)
-semantic = Model(input=input_u, output=g_u)
 
-# Layer to calc distance between generated vector on semantic embedding space
-# Joint learning
-distance = Lambda(mode_euclidean_distance, output_shape=(1,))([g_u, f_x])
+Ws = np.random.random((40,300))
+Ws[0] = np.ones(300)
+Wt = np.zeros((10,300))
 
-model = Model(input=[input_u, input_x], output=distance)
-# Compile model using function "nothing_to_lose"
-# I create this function, it only returns the result from the euclidean_distance
-model.compile(optimizer='sgd',
-              loss=nothing_to_lose)
+visual = Dense(300,
+               name='W_embedding',
+               activation='linear')(input_x)
 
-X = np.random.random((100,4096))
-U = np.random.random((100,300))
-Y = np.ones((100))
+sigal_layer = SigalLoss(Ws=Ws, Wt=Wt, C=0.01, alpha=0.01)
+f_x = sigal_layer([visual, input_u])
 
-model.fit([U, X], Y, nb_epoch=10)
+model = Model(input=([input_x, input_u]), output=f_x)
+model.compile(loss=nothing_to_lose, optimizer='sgd')
+
+X = np.random.random((100, 4096))
+A = np.random.normal(0.5, 1, (100,300))
+Y = np.zeros(100)
+
+model.fit([X,A], Y, nb_epoch=10, batch_size=1)
